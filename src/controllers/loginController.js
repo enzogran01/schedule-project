5e4137adf750470440317d585239a856680af284
enzogran01 <yournarrator013@gmail.com>
2026-03-23 10:16:27 -0300

    :tada: feat: creating users

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,3 +1,25 @@
+const Login = require('../models/LoginModel');
+
 exports.index = (req, res) => {
     res.render('login');
+};
+
+exports.register = async (req, res) => {
+    const login = new Login(req.body);
+    try {
+        await login.register();
+    
+        if(login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(() => res.redirect("/login/index"));
+            return;
+        }
+
+        req.flash('errors', "Seu usuário foi criado com sucesso");
+        req.session.save(() => res.redirect("/login/index"));
+    } catch (e) {
+        console.log(e);
+        res.render('404');
+    }
+
 }
