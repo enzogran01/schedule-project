9091b32d8fccaf6947c8b20403a1d1feba0580dd
enzogran01 <yournarrator013@gmail.com>
2026-03-23 14:43:02 -0300

    :tada: feat: login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,12 +1,13 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-    res.render('login');
+    if (req.session.user) return res.render('login-logado')
+    return res.render('login');
 };
 
 exports.register = async (req, res) => {
-    const login = new Login(req.body);
     try {
+        const login = new Login(req.body);
         await login.register();
     
         if(login.errors.length > 0) {
@@ -15,11 +16,35 @@ exports.register = async (req, res) => {
             return;
         }
 
-        req.flash('errors', "Seu usuário foi criado com sucesso");
+        req.flash('success', "Seu usuário foi criado com sucesso");
         req.session.save(() => res.redirect("/login/index"));
     } catch (e) {
         console.log(e);
         res.render('404');
     }
-
 }
+
+exports.login = async (req, res) => {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+    
+        if(login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(() => res.redirect("/login/index"));
+            return;
+        }
+
+        req.flash('success', "Você entrou no sistema");
+        req.session.user = login.user;
+        req.session.save(() => res.redirect("/login/index"));
+    } catch (e) {
+        console.log(e);
+        res.render('404');
+    }
+}
+
+exports.logout = (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+};
