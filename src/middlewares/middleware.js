2a82ea19c2f7dd48726b16c98658db172e4b17ae
enzogran01 <yournarrator013@gmail.com>
2026-03-23 09:19:08 -0300

    :tada: feat: login and register page

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -3,7 +3,7 @@ exports.middlewareGlobal = (req, res, next) => {
 };
 
 exports.checkCsrfError = (err, req, res, next) => {
-    if(err && 'EBADCSRFTOKEN' === err.code) {
+    if(err) {
         return res.render('404');
     };
     next();
