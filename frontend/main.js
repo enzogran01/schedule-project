b020e5d7c5c05305da89f7823666c81d3c5c4f64
enzogran01 <yournarrator013@gmail.com>
2026-03-25 11:07:48 -0300

    :recycle: refactor: frontend validation in login

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,11 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+import Login from './modules/Login';
+
+const cadastro = new Login('.form-cadastro');
+const login = new Login('.form-login');
+login.init();
+cadastro.init();
+
 // import './assets/css/style.css';
