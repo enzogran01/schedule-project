0eeda4407ce365d03097d55b4e75baf1530914e7
enzogran01 <yournarrator013@gmail.com>
2026-03-25 10:17:25 -0300

    :tada: feat: read and delete

--- routes.js
+++ routes.js
@@ -22,4 +22,7 @@ route.post('/contato/register', loginRequired, contatoController.register);
 route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
 route.post('/contato/edit/:id', loginRequired, contatoController.edit);
 
+// Rotas de delete
+route.get('/contato/delete/:id', loginRequired, contatoController.delete);
+
 module.exports = route;
